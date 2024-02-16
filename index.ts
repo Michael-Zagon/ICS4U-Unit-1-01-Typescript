bd6bcfe1fb0976912279fce08a5c4744c860afab
Michael <michael.zagon@mths.ca>
2024-02-16 15:01:58 +0000

    newday edits log program

---
+++ index.ts
@@ -0,0 +1,11 @@
+/**
+ * This program shows how much logs of each type can fit on a truck
+ *
+ * By Michael Zagon
+ * Version 1.0
+ * Since: 2024-02-14
+ */
+
+import { createPrompt } from 'bun-promptx'
+
+const numlogs = createprompt (Choose the length of the logs that you would like to move with a truck.
