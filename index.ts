26c79a1c9ca0b57860832ee297f07407a219b949
Michael <michael.zagon@mths.ca>
2024-02-18 04:37:37 +0000

    doneski

--- index.ts
+++ index.ts
@@ -8,4 +8,14 @@
 
 import { createPrompt } from 'bun-promptx'
 
-const numlogs = createprompt (Choose the length of the logs that you would like to move with a truck.
+const maxweight = 1100
+const weightperlog = 20
+
+const numlogs = createPrompt('Choose the length of the logs that you would like to move with a truck (in meters)? (0.25, 0.5 ,1):  ')
+
+let lengthoflogs = parseFloat(numlogs.value)
+let logamount = maxweight / (weightperlog * lengthoflogs)
+
+console.log(`The truck can carry ${logamount} logs if they are ${lengthoflogs}m long.`)
+
+console.log('\nDone')
